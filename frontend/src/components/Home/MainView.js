fae05ac4bf5239b45f3a6f79b0d4373410cc6cb6
Charlie Lu <charlielu05@gmail.com>
2022-11-05 06:21:56 +0000

    added search bar

--- frontend/src/components/Home/MainView.js
+++ frontend/src/components/Home/MainView.js
@@ -3,6 +3,7 @@ import React from "react";
 import agent from "../../agent";
 import { connect } from "react-redux";
 import { CHANGE_TAB } from "../../constants/actionTypes";
+import { useState } from "react"
 
 const YourFeedTab = (props) => {
   if (props.token) {
@@ -70,8 +71,27 @@ const mapDispatchToProps = (dispatch) => ({
 });
 
 const MainView = (props) => {
+  const [query, setquery] = useState('')
+  const [state, setstate] = useState({
+    query: '',
+    list: []
+  })
+  async function inputHandler(ev) {
+    setquery(ev.target.value);
+    console.log(ev.target.value);
+    const results = await agent.Items.filter(ev.target.value);
+    console.log(results);
+    setstate({
+      query: ev.target.value,
+      list: results
+    })
+    console.log(state.list.items)
+  }
+  
   return (
     <div>
+      <input type="text" id="search-box" placeholder="What is it that you truly desire?"
+        minLength="3" size="30" value = {query} onChange={inputHandler}></input>  
       <div className="feed-toggle">
         <ul className="nav nav-tabs">
           <YourFeedTab
@@ -85,10 +105,9 @@ const MainView = (props) => {
           <TagFilterTab tag={props.tag} />
         </ul>
       </div>
-
       <ItemList
         pager={props.pager}
-        items={props.items}
+        items={state.list.items}
         loading={props.loading}
         itemsCount={props.itemsCount}
         currentPage={props.currentPage}
